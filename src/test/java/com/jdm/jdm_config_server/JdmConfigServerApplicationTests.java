e8dbf41436fee23de6da60d3a1d0cbd852d222b3
Asan Indusara <asanindusara348@gmail.com>
2026-08-14 12:22:36 +0530

    feat: setup spring cloud config server with native config and actuator

---
+++ src/test/java/com/jdm/jdm_config_server/JdmConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdm.jdm_config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdmConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
